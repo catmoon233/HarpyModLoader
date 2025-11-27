f976d2b55027bd614709ee11f132bca13c7f77e8
agmas <Maxlion360@gmail.com>
2025-11-27 18:48:33 +0400

    allow viewing roles without a role

--- src/client/java/org/agmas/harpymodloader/client/mixin/CustomRolesRoleNameRendererMixin.java
+++ src/client/java/org/agmas/harpymodloader/client/mixin/CustomRolesRoleNameRendererMixin.java
@@ -27,7 +27,7 @@ public abstract class CustomRolesRoleNameRendererMixin {
     @Inject(method = "renderHud", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)I", ordinal = 0))
     private static void b(TextRenderer renderer, ClientPlayerEntity player, DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
          GameWorldComponent gameWorldComponent = (GameWorldComponent) GameWorldComponent.KEY.get(player.getWorld());
-         if (HarpymodloaderClient.hudRole != null && gameWorldComponent.getRole(player) != null) {
+         if (HarpymodloaderClient.hudRole != null) {
              if (TMMClient.isPlayerSpectatingOrCreative()) {
                  Text name = Text.translatable("announcement.role." + HarpymodloaderClient.hudRole.identifier().getPath());
                  context.drawTextWithShadow(renderer, name, -renderer.getWidth(name) / 2, 0, HarpymodloaderClient.hudRole.color() | (int) (nametagAlpha * 255.0F) << 24);
